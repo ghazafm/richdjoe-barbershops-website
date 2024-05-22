860ca2724ae9f34ce869fb8811459ade73e813af
ghazafm <fauzan.ghaza@gmail.com>
2024-05-22 09:05:06 +0700

    update migration

--- database/migrations/2024_05_16_054028_create_transactions_table.php
+++ database/migrations/2024_05_16_054028_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('kapster_id');
             $table->unsignedBigInteger('service_id');
             $table->enum('type', ['haircut', 'other']);
