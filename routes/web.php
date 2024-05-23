1d37630616d1ae675f5c0bb0816e7e08022b51c1
ghazafm <fauzan.ghaza@gmail.com>
2024-05-23 18:24:34 +0700

    fix book controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\UserBookController;
+use App\Http\Controllers\User\UserBookController;
 use App\Http\Controllers\Malicious\MaliciousController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
