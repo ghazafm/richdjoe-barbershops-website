821a2df01f60eb235ed77511121a0b4cd2524aa5
Ghaza <159403697+ghazafm@users.noreply.github.com>
2024-05-24 04:14:34 +0700

    Revert "Admin feature"

--- app/Http/Controllers/Admin/AdminBookController.php
+++ app/Http/Controllers/Admin/AdminBookController.php
@@ -20,11 +20,6 @@ class AdminBookController extends Controller
 		return view('admin.book', ['transaction' => $transactions]);
 	}
 
-	public function detail($id)
-	{
-		$transaction = Transaction::find($id);
-        return view('admin.book_detail', compact('transaction'));
-	}
 	public function booksave(Request $req)
 	{
 		$validated = $req->validate([
