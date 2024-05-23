41723131deefd74c701c74cd7dc4f2b21b212266
Awan <lukasawancb@gmail.com>
2024-05-23 21:02:22 +0700

    icon-dashboard-done

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 <div class="d-flex align-items-stretch">
     <!-- Sidebar Navigation-->
     <nav id="sidebar">
@@ -5,28 +7,17 @@
       <div class="sidebar-header d-flex align-items-center">
         <div class="avatar"><img src="{{asset('/admincss/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
         <div class="title">
-          <h1 class="h5">Mark Stephen</h1>
-          <p>Web Designer</p>
+          <h1 class="h5">Bimo</h1>
+          <p>CEO Richdjoe</p>
         </div>
       </div>
       <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
       <ul class="list-unstyled">
-              <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
-              <li><a href="tables.html"> <i class="icon-grid"></i>Tables </a></li>
-              <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
-              <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
-              <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
-                <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                  <li><a href="#">Page</a></li>
-                  <li><a href="#">Page</a></li>
-                  <li><a href="#">Page</a></li>
-                </ul>
-              </li>
-              <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
-      </ul><span class="heading">Extras</span>
-      <ul class="list-unstyled">
-        <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
-        <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
-        <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>
-      </ul>
+        <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+        <li><a href="book.html"> <i class="icon-grid"></i>Book </a></li>
+        <li><a href="service.html"> <i class="fa fa-scissors"></i>Service </a></li>
+        <li><a href="hairartist.html"> <i class="fa fa-user-tie"></i>HairArtist</a></li>
+        <li><a href="user.html"> <i class="fa fa-user"></i>User</a></li>
+    </ul>
+    
     </nav>
