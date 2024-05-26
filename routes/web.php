448289358c146468b8cd5ae18a6804560a4a40fd
ghazafm <fauzan.ghaza@gmail.com>
2024-05-26 16:24:51 +0700

    solve admin issue

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,12 @@ Route::get('/admin/dashboard', [AdminController::class, 'index'])->middleware(['
 Route::get('/admin/book/add', [AdminBookController::class, 'book'])->middleware(['auth', 'admin']);
 Route::get('/admin/book', [AdminBookController::class, 'book'])->middleware(['auth', 'admin']);
 Route::get('/admin/book/{id}', [AdminBookController::class, 'detail_book'])->middleware(['auth', 'admin']);
+Route::post('/admin/book/verify/{id}', [AdminBookController::class, 'verify_service'])->name('admin.book.verify');
+Route::post('/admin/book/decline/{id}', [AdminBookController::class, 'decline_service'])->name('admin.book.decline');
 Route::get('/admin/book/add', [AdminBookController::class, 'add'])->middleware(['auth', 'admin']);
 Route::post('/admin/book/addsave', [AdminBookController::class, 'addsave'])->middleware(['auth', 'admin']);
+Route::get('/admin/book/asc', 'Admin\AdminBookController@book_asc')->name('book_asc');
+Route::get('/admin/book/sort', 'Admin\AdminBookController@sort')->name('sort_book');
 Route::get('/admin/service', [AdminServiceController::class, 'index'])->middleware(['auth', 'admin']);
 Route::get('/admin/hairartist', [AdminKapsterController::class, 'index'])->middleware(['auth', 'admin']);
 Route::get('/admin/hairartist/edit/{id}', [AdminKapsterController::class, 'edit'])->middleware(['auth', 'admin']);
