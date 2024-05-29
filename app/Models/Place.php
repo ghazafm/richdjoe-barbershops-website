2f953c0da4cb71a5f08161cd768d208b33c652f4
ghazafm <fauzan.ghaza@gmail.com>
2024-05-29 22:53:22 +0700

    solve add place database

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Place extends Model
 {
     use HasFactory;
+
+    protected $table = 'places';
+
+    // Define the fillable attributes
+    protected $fillable = [
+        'name',
+        'address',
+    ];
+
+    // Disable timestamps if your table does not have created_at and updated_at columns automatically managed
+    public $timestamps = true;
 }
