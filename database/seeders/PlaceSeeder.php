606da31dd0ed5ee6a90fc3a1e4889af4db6890ac
azril <azriljanuar661@gmail.com>
2024-05-30 17:29:52 +0700

    place-seeder

--- database/seeders/PlaceSeeder.php
+++ database/seeders/PlaceSeeder.php
@@ -13,9 +13,9 @@ class PlaceSeeder extends Seeder
     public function run(): void
     {
         $places = [
-            ['name' => 'Ijen', 'address' => ''],
-            ['name' => 'Sigura-gura', 'address' => ''],
-            ['name' => 'Coklat', 'address' => ''],
+            ['name' => 'Ijen', 'address' => 'Jl. Besar Ijen No.86, Oro-oro Dowo, Kec. Klojen, Kota Malang, Jawa Timur 65119'],
+            ['name' => 'Sigura-gura', 'address' => 'Jl. Sigura - Gura No.2 Kavling 4, Sumbersari, Kec. Lowokwaru, Kota Malang, Jawa Timur 65145'],
+            ['name' => 'Soekarno Hatta', 'address' => 'Jl. Bunga Coklat No.1, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141'],
         ];
 
         foreach ($places as $place) {
